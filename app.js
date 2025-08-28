582279e028d2cf3c0514eb3095a25e334bc79a5a
polokrf <polokkumar9030@gmail.com>
2025-08-29 05:28:01 +0600

    my first commit

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ document.getElementById('card-main').addEventListener('click', function (e) {
     }
 
     let createDiv = document.createElement('div');
-    createDiv.innerHTML = `<div  class=" flex justify-between items-center p-[15px] my-[25px] bg-slate-300 rounded-md shadow-md">
+    createDiv.innerHTML = `<div  class=" flex justify-between items-center p-[15px] my-[25px] bg-[#FAFAFA] rounded-md shadow-md">
             <div>
               <h2 class="text-[20px] font-bold">${mainTitle}</h2>
               <p class="font-bold text-[#5C5C5C]">${helpNumber}</p>
